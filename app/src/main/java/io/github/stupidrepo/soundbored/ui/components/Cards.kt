c6a3b596bca54069691ca6521223008bf701da23
Bradlee Barnes <69256931+StupidRepo@users.noreply.github.com>
2024-11-10 13:27:25 +0000

    repurpose compact sound card into provider card

--- app/src/main/java/io/github/stupidrepo/soundbored/ui/components/Cards.kt
+++ app/src/main/java/io/github/stupidrepo/soundbored/ui/components/Cards.kt
@@ -1,28 +1,26 @@
 package io.github.stupidrepo.soundbored.ui.components
 
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Favorite
 import androidx.compose.material.icons.rounded.FavoriteBorder
 import androidx.compose.material.icons.rounded.PlayCircle
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -30,13 +28,19 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.ExperimentalTextApi
+import androidx.compose.ui.text.LinkAnnotation
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextLinkStyles
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.text.withLink
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import io.github.stupidrepo.soundbored.SoundboredApplication
+import io.github.stupidrepo.soundbored.providers.IProvider
 import io.github.stupidrepo.soundbored.retrofit.GenericSound
-import io.github.stupidrepo.soundbored.providers.sbw.Sound
 
 fun getColour(name: String): Color = when(name) {
     "red" -> Color.hsv(0f, 0.8f, 0.8f)
@@ -56,7 +60,7 @@ fun getColour(name: String): Color = when(name) {
 fun OurCard(modifier: Modifier = Modifier, borderStroke: BorderStroke, content: @Composable ColumnScope.() -> Unit) {
     OutlinedCard (
         modifier = modifier
-            .padding(4.dp).fillMaxSize(),
+            .padding(4.dp),
         border = borderStroke
     ) {
         Column(
@@ -78,7 +82,7 @@ fun SoundboardCard(sound: GenericSound, onExpand: (GenericSound) -> Unit, onPlay
     OurCard(
         modifier = Modifier
             .clickable(onClick = { onExpand(sound) })
-            .fillMaxWidth(),
+            .fillMaxWidth().fillMaxSize(),
         borderStroke = BorderStroke(2.dp, getColour(sound.color))
     ) {
         // make sound icon, with tint according the sound colour
@@ -117,48 +121,50 @@ fun SoundboardCard(sound: GenericSound, onExpand: (GenericSound) -> Unit, onPlay
 }
 
 // Compact
-//@Composable
-//fun SoundboardCard(sound: GenericSound, onExpand: (GenericSound) -> Unit, onPlay: () -> Unit) {
-//    OurCard(
-//        modifier = Modifier
-//            .clickable(onClick = { onExpand(sound) })
-//            .fillMaxWidth().wrapContentHeight(),
-//        borderStroke = BorderStroke(2.dp, getColour(sound.color))
-//    ) {
-//        Row(
-//            verticalAlignment = Alignment.CenterVertically,
-//            horizontalArrangement = Arrangement.SpaceBetween,
-//        ) {
-//            // make sound icon, with tint according the sound colour
-//            IconButton(onClick = onPlay, modifier = Modifier.size(48.dp)) {
-//                Icon(
-//                    imageVector = Icons.Rounded.PlayCircle,
-//                    contentDescription = "Play Sound Icon",
-//                    tint = MaterialTheme.colorScheme.inverseSurface,
-//                    modifier = Modifier.fillMaxSize()
-//                )
-//            }
-//
-//            // make sound title
-//            Text(
-//                sound.name.lowercase(),
-//                overflow = TextOverflow.Ellipsis,
-//                maxLines = 2,
-//                textAlign = TextAlign.Center,
-//                modifier = Modifier.weight(1f).padding(4.dp)
-//            )
-//
-//            IconButton(onClick = {}, modifier = Modifier.size(28.dp)) {
-//                Icon(
-//                    imageVector = if(sound.isFavourited()) Icons.Rounded.Favorite else Icons.Rounded.FavoriteBorder,
-//                    contentDescription = "Favourite Icon",
-//                    tint = MaterialTheme.colorScheme.inverseSurface,
-//                    modifier = Modifier.fillMaxSize()
-//                )
-//            }
-//        }
-//    }
-//}
+@OptIn(ExperimentalTextApi::class)
+@Composable
+fun ProviderCard(provider: IProvider, isChecked: Boolean, onSelect: () -> Unit) {
+    OutlinedCard (
+        modifier = Modifier
+            .padding(4.dp)
+            .fillMaxWidth(),
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .padding(8.dp)
+        ) {
+            Checkbox(checked = isChecked, onCheckedChange = { onSelect() })
+
+            Column {
+                Text(
+                    provider.getProviderName(),
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 1,
+                )
+
+                Text(
+                    AnnotatedString.Builder().apply {
+                        withLink(
+                            LinkAnnotation.Url(
+                                provider.getProviderURL(),
+                                TextLinkStyles(style = SpanStyle(color = Color.Cyan))
+                            )
+                        ) {
+                            append(provider.getProviderURL())
+                        }
+
+                    }.toAnnotatedString(),
+                    modifier = Modifier.fillMaxWidth(),
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 1,
+                    style = typography.bodySmall
+                )
+            }
+        }
+    }
+}
 
 
 @Composable
